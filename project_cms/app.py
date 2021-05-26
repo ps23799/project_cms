262b71cd457f1485761f7ee4e8de7bba4043ecb5
ps23799 <54246779+ps23799@users.noreply.github.com>
2021-05-26 19:33:49 +0530

    first_commit

---
+++ project_cms/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
